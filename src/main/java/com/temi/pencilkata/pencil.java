7be9e4c44703b2bfdd3768712058286ea249551c
Temitope Jegede <temiteejegede@gmail.com>
2022-06-03 15:53:52 -0400

    Adding tests and more functionality

--- src/main/java/com/temi/pencilkata/pencil.java
+++ src/main/java/com/temi/pencilkata/pencil.java
@@ -5,25 +5,57 @@ public class pencil {
     private int inkLeft;
     private int length;
     private int eraserDurability = 5;
-    private int eraserLeft;
+    private int eraserLeft = 5;
 
     private String paper;
 
     public pencil(int length) {
         this.length = length;
+        this.inkLeft = length;
     }
 
     public pencil(int pencilDurability, int length) {
         this.pencilDurability = pencilDurability;
         this.length = length;
+        this.inkLeft = length;
     }
 
     public pencil(int pencilDurability, int length, int eraserDurability) {
         this.pencilDurability = pencilDurability;
         this.length = length;
         this.eraserDurability = eraserDurability;
+        this.inkLeft = length;
     }
 
+    public void write(String word){
+        if(inkLeft == 0) paper += word.replaceAll("a-zA-z0-9", " ");
+
+        else if(inkLeft > 0 && inkLeft >= word.length()){
+            char[] ans = word.toCharArray();
+            for(char s: ans){
+                if(Character.isUpperCase(s)){
+                    inkLeft -= 2;
+                } else inkLeft--;
+                paper += s;
+            }
+        }
+
+        else if(inkLeft < word.length()){
+            paper+= word.substring(0, inkLeft -1);
+            paper += word.substring(inkLeft).replaceAll("a-zA-Z0-9", " ");
+            inkLeft = 0;
+        }
+    }
+
+    public void sharpen(){
+        if(length == 0) return;
+        inkLeft = pencilDurability;
+        length--;
+    }
+
+    public void erase(){
+
+    }
 
 
 }
