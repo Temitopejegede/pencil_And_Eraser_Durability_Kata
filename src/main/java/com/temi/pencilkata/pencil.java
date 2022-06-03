defb477f0bc057930688a197e920708d5af4478d
Temitope Jegede <temiteejegede@gmail.com>
2022-06-03 12:58:15 -0400

    Initial commit

---
+++ src/main/java/com/temi/pencilkata/pencil.java
@@ -0,0 +1,5 @@
+package com.temi.pencilkata;
+
+public class pencil {
+
+}
---
+++ src/test/java/com/temi/pencilKata/pencilTest.java
@@ -0,0 +1,7 @@
+package com.temi.pencilKata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class pencilTest {
+
+}
